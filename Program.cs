b93319dc558ae5502eb679b34a0f5e37cadf1d92
Fodor-Levente-Gabor <fodorl@kkszki.hu>
2025-09-19 15:14:07 +0200

    Half of the tasks are done

---
+++ Program.cs
@@ -0,0 +1,202 @@
+using System;
+
+class Program 
+{
+    static void Main(string[] args)
+    {
+        string[] tasks = {"Páros vagy páratlan szám", "Pozitív, negatív vagy nulla", "Évszak meghatározása hónap alapján", "Szavazati jog eldöntése", "Jeles vagy elégtelen", "Számkitaláló – egyenlő vagy nagyobb/kisebb", "Páros és pozitív ellenőrzés", "Betű vagy szám karakter", "Belépés jelszóval", "Három szám közül a legnagyobb", "Három szám közül a legnagyobb"};
+        bool run = true;
+        int num1 = 0;
+        while (run)
+        { 
+            for (int a = 0; a < 44; a++)
+            {
+                Console.Write("-");
+            }
+            Console.WriteLine("-");
+            for (int a = 0; a < 10; a++)
+            {
+                Console.WriteLine($"{a+1}. {tasks[a]}");
+                
+                
+            }
+            Console.WriteLine("0. Program bezárása");
+            Console.Write("Válassz műveletet számmal: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            
+            for (int a = 0; a < 44; a++)
+            {
+                Console.Write("-");
+            }
+            Console.WriteLine("-");
+             
+            //Console.Clear();
+            switch (num1)
+            {
+                case 0:
+                    run = false;
+                    break;
+                case 1:
+                    Function1();
+                    break;
+                case 2:
+                    Function2();
+                    break;
+                case 3:
+                    Function3();
+                    break;
+                case 4:
+                    Function4();
+                    break;
+                case 5:
+                    Function5();
+                    break;
+                case 6:
+                    Function6();
+                    break;
+                case 7:
+                    Function7();
+                    break;
+                case 8:
+                    Function8();
+                    break;
+                case 9:
+                    Function9();
+                    break;
+                case 10:
+                    Function10();
+                    break;
+            }
+        }
+    }
+
+    static void Function1() {
+        Console.Write("Adj meg egy számot: ");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+        if (num2 % 2 == 0) 
+        {
+            Console.WriteLine($"{num2} páros szám.");
+        }
+        else
+        {
+            Console.WriteLine($"{num2} páratlan szám.");
+        }
+    }
+    static void Function2() {
+        Console.Write("Adj meg egy számot: ");
+        int num3 = Convert.ToInt32(Console.ReadLine());
+        if (num3 == 0)
+        {
+            Console.WriteLine($"{num3} nulla.");
+        }
+        else if (num3 > 0)
+        {
+            Console.WriteLine($"{num3} pozitív szám.");
+        }
+        else
+        { 
+            Console.WriteLine($"{num3} negatív szám.");
+        }
+    }
+    static void Function3() {
+        Console.Write("Adj meg egy számot, majd kiírom hogy melyik hónap: ");
+        int num4 = Convert.ToInt32(Console.ReadLine());
+        if (num4 == 1)
+        {
+            Console.WriteLine($"{num4}. hónap Január.");
+        }
+        else if (num4 == 2)
+        {
+            Console.WriteLine($"{num4}. hónap Február.");
+        }
+        else if (num4 == 3)
+        {
+            Console.WriteLine($"{num4}. hónap Március.");
+        }
+        else if (num4 == 4)
+        {
+            Console.WriteLine($"{num4}. hónap Április.");
+        }
+        else if (num4 == 5)
+        {
+            Console.WriteLine($"{num4}. hónap Május.");
+        }
+        else if (num4 == 6)
+        {
+            Console.WriteLine($"{num4}. hónap Június.");
+        }
+        else if (num4 == 7)
+        {
+            Console.WriteLine($"{num4}. hónap Július.");
+        }
+        else if (num4 == 8)
+        {
+            Console.WriteLine($"{num4}. hónap Augusztus.");
+        }
+        else if (num4 == 9)
+        {
+            Console.WriteLine($"{num4}. hónap Szeptember.");
+        }
+        else if (num4 == 10)
+        {
+            Console.WriteLine($"{num4}. hónap Október.");
+        }
+        else if (num4 == 11)
+        {
+            Console.WriteLine($"{num4}. hónap November.");
+        }
+        else if (num4 == 12)
+        {
+            Console.WriteLine($"{num4}. hónap December.");
+        }
+    }
+    static void Function4() {
+        Console.Write("Add meg hány éves vagy: ");
+        int num5 = Convert.ToInt32(Console.ReadLine());
+        if (num5 < 18 )
+        {
+            Console.WriteLine($"{num5} évesen nincs jogod szavazni.");
+        }
+        else
+        { 
+            Console.WriteLine($"{num5} van szavazati jogod.");
+        }
+    }
+    static void Function5()
+    {
+        Console.Write("Adj meg egy érdem jegyet: ");
+        int num6 = Convert.ToInt32(Console.ReadLine());
+        if (num6 == 1 || num6 == 2)
+        {
+            Console.WriteLine("Elégtelen.");
+        }
+        else if (num6 == 3)
+        {
+            Console.WriteLine("Elégséges.");
+        }
+        else if (num6 == 4)
+        {
+            Console.WriteLine("Jó.");
+        }
+        else
+        {
+            Console.WriteLine("Jeles.");
+        }
+    }
+    static void Function6() { 
+        
+    }
+    static void Function7() { 
+        
+    }
+    static void Function8() { 
+        
+    }
+    static void Function9() { 
+        
+    }
+    static void Function10() { 
+        
+    }
+    
+}
